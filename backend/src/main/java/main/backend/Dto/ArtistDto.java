120cfad6ce136030b9e89eb986971328dee7f8af
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:31:34 +0100

    Added EventDto

--- backend/src/main/java/main/backend/Dto/ArtistDto.java
+++ backend/src/main/java/main/backend/Dto/ArtistDto.java
@@ -1,7 +1,5 @@
 package main.backend.Dto;
 
-import jakarta.persistence.Column;
-
 public class ArtistDto {
     private String firstname;
     private String lastname;
---
+++ backend/src/main/java/main/backend/Dto/EventDto.java
@@ -0,0 +1,10 @@
+package main.backend.Dto;
+
+import java.time.LocalDate;
+
+public class EventDto {
+    private String title;
+    private String location;
+    private LocalDate eventDate;
+    private String imageUrl;
+}
