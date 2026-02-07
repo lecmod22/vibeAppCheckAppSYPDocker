05706a43f69a345f57fbc63e0f55fb47df6d02e3
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:54:12 +0100

    Added annotations to EventDto

--- backend/src/main/java/main/backend/Dto/EventDto.java
+++ backend/src/main/java/main/backend/Dto/EventDto.java
@@ -1,10 +1,17 @@
 package main.backend.Dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@Data
 public class EventDto {
     private String title;
     private String location;
     private LocalDate eventDate;
     private String imageUrl;
+    private double avgRating;
+    private long ratingCount;
 }
