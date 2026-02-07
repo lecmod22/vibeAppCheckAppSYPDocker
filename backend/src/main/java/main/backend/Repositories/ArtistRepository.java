5a781635e0d8c3e48ac8a5129d462b3df16d56c9
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:23:24 +0100

    Added Artist Repository

---
+++ backend/src/main/java/main/backend/Repositories/ArtistRepository.java
@@ -0,0 +1,9 @@
+package main.backend.Repositories;
+
+import main.backend.Pojo.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtistRepository extends JpaRepository<Artist, Long> {
+}
