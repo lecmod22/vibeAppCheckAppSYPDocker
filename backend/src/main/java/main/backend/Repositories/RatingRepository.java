cb03f23adb0565f98752dc8c95d14a26eeac93d5
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:24:07 +0100

    Added Rating Repository

---
+++ backend/src/main/java/main/backend/Repositories/RatingRepository.java
@@ -0,0 +1,9 @@
+package main.backend.Repositories;
+
+import main.backend.Pojo.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating,Long> {
+}
