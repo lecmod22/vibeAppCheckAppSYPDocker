75f0da2aec467171bda70b8d4428a5fda99fa5e2
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:24:43 +0100

    Added Event Repository

---
+++ backend/src/main/java/main/backend/Repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package main.backend.Repositories;
+
+import main.backend.Pojo.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long> {
+}
