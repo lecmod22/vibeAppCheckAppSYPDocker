4191f6190814038c32c95d499d15e47191889361
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:54:53 +0100

    Added Pageable to EvenRepository for pagination in backend

--- backend/src/main/java/main/backend/Repositories/EventRepository.java
+++ backend/src/main/java/main/backend/Repositories/EventRepository.java
@@ -1,9 +1,12 @@
 package main.backend.Repositories;
 
 import main.backend.Pojo.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event,Long> {
+    Page<Event> findDistinctByArtists_Id(Long artistId, Pageable pageable);
 }
