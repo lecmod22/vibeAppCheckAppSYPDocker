f1a440f5e8215e4a39c94d7421c8e9e9e02a69fb
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:32:23 +0100

    Added RatingDto

---
+++ backend/src/main/java/main/backend/Dto/RatingDto.java
@@ -0,0 +1,9 @@
+package main.backend.Dto;
+
+import java.time.LocalDateTime;
+
+public class RatingDto {
+    private int stars;
+    private String comment;
+    private LocalDateTime createdAt;
+}
