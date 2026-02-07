b0870738a3e731416336477527dcec13e39ac4d5
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:54:26 +0100

    Added annotations to RatingDto

--- backend/src/main/java/main/backend/Dto/RatingDto.java
+++ backend/src/main/java/main/backend/Dto/RatingDto.java
@@ -1,7 +1,12 @@
 package main.backend.Dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Data
 public class RatingDto {
     private int stars;
     private String comment;
