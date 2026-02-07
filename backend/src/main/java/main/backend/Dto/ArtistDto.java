354fa263e1bb4af895ceb330361bbfa9d04e560f
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-07 12:29:47 +0100

    Added ArtistDto

---
+++ backend/src/main/java/main/backend/Dto/ArtistDto.java
@@ -0,0 +1,10 @@
+package main.backend.Dto;
+
+import jakarta.persistence.Column;
+
+public class ArtistDto {
+    private String firstname;
+    private String lastname;
+    private String description;
+    private String imageUrl;
+}
