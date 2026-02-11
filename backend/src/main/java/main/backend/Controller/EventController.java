20cdd1026056e9396a4d21c7cf0c34e2f3044876
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:19:54 +0100

    added RequiredArgsConstructor annotation to EventController

--- backend/src/main/java/main/backend/Controller/EventController.java
+++ backend/src/main/java/main/backend/Controller/EventController.java
@@ -1,9 +1,11 @@
 package main.backend.Controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/events")
+@RequiredArgsConstructor
 public class EventController {
 }
