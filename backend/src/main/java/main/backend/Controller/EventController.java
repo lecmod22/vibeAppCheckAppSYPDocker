5a3bde9888918bef15c42055412deee226bb2d13
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:22:48 +0100

    added getAllEvents to EventController

--- backend/src/main/java/main/backend/Controller/EventController.java
+++ backend/src/main/java/main/backend/Controller/EventController.java
@@ -1,11 +1,26 @@
 package main.backend.Controller;
 
 import lombok.RequiredArgsConstructor;
+import main.backend.Dto.ArtistDto;
+import main.backend.Dto.EventDto;
+import main.backend.Services.EventService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
 public class EventController {
+    private final EventService eventService;
+
+    @GetMapping("/events")
+    public ResponseEntity<Page<EventDto>> getAllEvents(Pageable pageable) {
+        return ResponseEntity.ok(eventService.getAllEvents(pageable));
+    }
 }
