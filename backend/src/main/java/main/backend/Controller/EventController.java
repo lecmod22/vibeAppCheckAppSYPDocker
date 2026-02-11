8d11ded283b727c1b3c209a16e5fca463bff68e2
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:23:52 +0100

    removed unnecessiary imports

--- backend/src/main/java/main/backend/Controller/EventController.java
+++ backend/src/main/java/main/backend/Controller/EventController.java
@@ -1,7 +1,6 @@
 package main.backend.Controller;
 
 import lombok.RequiredArgsConstructor;
-import main.backend.Dto.ArtistDto;
 import main.backend.Dto.EventDto;
 import main.backend.Services.EventService;
 import org.springframework.data.domain.Page;
@@ -10,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
