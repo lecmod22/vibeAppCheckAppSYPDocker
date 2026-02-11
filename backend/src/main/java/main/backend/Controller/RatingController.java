2557cf1dc85207e1f9f3e9a4d0227b759bc88dfb
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:30:01 +0100

    Added RequestMapping annotation to RatingController

--- backend/src/main/java/main/backend/Controller/RatingController.java
+++ backend/src/main/java/main/backend/Controller/RatingController.java
@@ -1,7 +1,9 @@
 package main.backend.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/ratings")
 public class RatingController {
 }
