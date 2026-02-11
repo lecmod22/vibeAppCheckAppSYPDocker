ce35a53d367ebf9200408a218f960fbf9a3f0421
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:31:22 +0100

    Added getRatingsForEvent to RatingController

--- backend/src/main/java/main/backend/Controller/RatingController.java
+++ backend/src/main/java/main/backend/Controller/RatingController.java
@@ -1,11 +1,24 @@
 package main.backend.Controller;
 
 import lombok.RequiredArgsConstructor;
+import main.backend.Dto.RatingDto;
+import main.backend.Services.RatingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/ratings")
 @RequiredArgsConstructor
 public class RatingController {
+    private final RatingService ratingService;
+
+    @GetMapping("/api/ratings/event/{eventId}")
+    public ResponseEntity<List<RatingDto>> getRatingsForEvent(@PathVariable Long eventId) {
+        return ResponseEntity.ok(ratingService.getRatingsForEvent(eventId));
+    }
 }
