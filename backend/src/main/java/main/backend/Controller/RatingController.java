32ca537496667b309eff09308090fb9f2ad9d496
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:30:27 +0100

    Added RequiredArgsConstructor annotation to RatingController

--- backend/src/main/java/main/backend/Controller/RatingController.java
+++ backend/src/main/java/main/backend/Controller/RatingController.java
@@ -1,9 +1,11 @@
 package main.backend.Controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/ratings")
+@RequiredArgsConstructor
 public class RatingController {
 }
