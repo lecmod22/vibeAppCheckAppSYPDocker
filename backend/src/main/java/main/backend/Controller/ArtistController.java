0853a43a27bd0973b43bbf62e9766e2a70973743
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-11 11:14:19 +0100

    Added getAllArtist to ArtistController

--- backend/src/main/java/main/backend/Controller/ArtistController.java
+++ backend/src/main/java/main/backend/Controller/ArtistController.java
@@ -1,11 +1,24 @@
 package main.backend.Controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import main.backend.Dto.ArtistDto;
+import main.backend.Pojo.Artist;
+import main.backend.Services.ArtistService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/artists")
 @RequiredArgsConstructor
 public class ArtistController {
+    private final ArtistService artistService;
+
+    @GetMapping("/artists")
+    public List<ArtistDto> getAllArtists() {
+        return artistService.getAllArtists();
+    }
 }
