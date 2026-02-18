2bd4dc24bf85e49b301c23a2b4b5caace228c3cd
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-18 09:47:33 +0100

    Added RequiredArgsConstructor annotation to InitDatabase

--- backend/src/main/java/main/backend/Database/InitDatabase.java
+++ backend/src/main/java/main/backend/Database/InitDatabase.java
@@ -1,9 +1,11 @@
 package main.backend.Database;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class InitDatabase {
 }
