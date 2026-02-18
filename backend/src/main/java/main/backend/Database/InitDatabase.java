e3fc20afdd833ffbe9968593f7070d35e04c9742
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-18 09:48:11 +0100

    Added needed Repositories to InitDatabase

--- backend/src/main/java/main/backend/Database/InitDatabase.java
+++ backend/src/main/java/main/backend/Database/InitDatabase.java
@@ -2,10 +2,16 @@ package main.backend.Database;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import main.backend.Repositories.ArtistRepository;
+import main.backend.Repositories.EventRepository;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class InitDatabase {
+
+    private final ArtistRepository artistRepository;
+    private final EventRepository eventRepository;
+
 }
