e417b424f2c63d7f2bec7f0b6da9a12ef3052033
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 08:49:50 +0100

    added getArtistById to artistService.ts

--- frontend/src/services/artistService.ts
+++ frontend/src/services/artistService.ts
@@ -5,3 +5,8 @@ export const getArtists = async (): Promise<Artist[]> => {
     const res = await apiClient.get("/api/artists");
     return res.data;
 };
+
+export const getArtistById = async (artistId: number): Promise<Artist> => {
+    const res = await apiClient.get(`/api/artists/${artistId}`);
+    return res.data;
+};
