a9e9ea0c4381dca76175b49c54eddf5230728ec6
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 08:36:24 +0100

    added Rating type in model.ts

--- frontend/src/common/model.ts
+++ frontend/src/common/model.ts
@@ -10,9 +10,16 @@ export type Event = {
     id: number;
     title: string;
     location: string;
-    eventDate: string; // ISO string (LocalDate from backend)
+    eventDate: string;
     imageUrl: string;
     avgRating: number;
     ratingCount: number;
     artists?: Artist[];
+};
+
+export type Rating = {
+    id: number;
+    stars: number;
+    comment: string;
+    createdAt: string;
 };
