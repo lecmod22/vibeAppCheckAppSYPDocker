2adb1768ffdfe94a693f9661317ba7ce8651800c
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 08:36:01 +0100

    added Event type in model.ts

--- frontend/src/common/model.ts
+++ frontend/src/common/model.ts
@@ -5,3 +5,14 @@ export type Artist = {
     description: string;
     imageUrl: string;
 };
+
+export type Event = {
+    id: number;
+    title: string;
+    location: string;
+    eventDate: string; // ISO string (LocalDate from backend)
+    imageUrl: string;
+    avgRating: number;
+    ratingCount: number;
+    artists?: Artist[];
+};
