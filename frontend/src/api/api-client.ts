2ad5dd0c7a601ca7ed772e44c5ba0d3043a554f0
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 08:40:11 +0100

    Added api-client.ts

---
+++ frontend/src/api/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "https://localhost:8081",
+    headers: { "Content-Type": "application/json" },
+});
+
+export default apiClient;
