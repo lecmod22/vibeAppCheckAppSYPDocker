d78fd5125b344f8f6949bcf05ab6fe181084d5e9
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 09:04:39 +0100

    added fetchEvents to eventStore

--- frontend/src/stores/eventStore.ts
+++ frontend/src/stores/eventStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import type { Event } from "../common/model";
+import {getEvents} from "../services/eventService.ts";
 
 
 type EventStore = {
@@ -13,9 +14,11 @@ type EventStore = {
 
     loading: boolean;
     error: string | null;
+
+    fetchEvents: () => Promise<void>;
 };
 
-export const useEventStore = create<EventStore>(() => ({
+export const useEventStore = create<EventStore>((set, get) => ({
     events: [],
     selectedEvent: null,
 
@@ -25,5 +28,24 @@ export const useEventStore = create<EventStore>(() => ({
     sort: "eventDate,asc",
 
     loading: false,
-    error: null
+    error: null,
+
+    fetchEvents: async () => {
+        const { page, size, sort } = get();
+        set({ loading: true, error: null });
+
+        try {
+            const data = await getEvents({ page, size, sort });
+            set({
+                events: data,
+                loading: false,
+            });
+        } catch (err) {
+            console.log(err)
+            set({
+                error: "Failed to load events",});
+        } finally {
+            set({loading: false})
+        }
+    },
 }));
