f0ad5cb2adb927814848988518747db79f4036e9
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 08:54:53 +0100

    added createRating to ratingService.ts

--- frontend/src/services/ratingService.ts
+++ frontend/src/services/ratingService.ts
@@ -1,8 +1,16 @@
-import type { Rating } from "../common/model";
+import type {CreateRatingDto, Rating} from "../common/model";
 import apiClient from "../api/api-client";
 
 
 export const getRatingsByEvent = async (eventId: number): Promise<Rating[]> => {
     const res = await apiClient.get(`/api/ratings/event/${eventId}`);
+    return res.data;
+};
+
+export const createRating = async (
+    eventId: number,
+    data: CreateRatingDto
+): Promise<Rating> => {
+    const res = await apiClient.post(`/api/ratings/${eventId}`, data);
     return res.data;
 };
