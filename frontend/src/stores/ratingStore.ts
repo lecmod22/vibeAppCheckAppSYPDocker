86c3d6120042007d51e5b62de392a0f58ab26418
lecmod22 <lecmod22@htl-kaindorf.at>
2026-02-23 09:16:04 +0100

    added fetchRatings to ratingStore.ts

--- frontend/src/stores/ratingStore.ts
+++ frontend/src/stores/ratingStore.ts
@@ -1,15 +1,32 @@
 import { create } from "zustand";
 import type {Rating } from "../common/model";
+import {getRatingsByEvent} from "../services/ratingService.ts";
 
 type RatingStore = {
     ratings: Rating[];
 
     loading: boolean;
     error: string|null;
+
+    fetchRatings: (eventId: number) => Promise<void>;
 }
 
-export const useRatingStore = create<RatingStore>(() => ({
+export const useRatingStore = create<RatingStore>((set) => ({
     ratings: [],
+
     loading: false,
-    error: null
+    error: null,
+
+    fetchRatings: async (eventId: number) => {
+        set({ loading: true, error: null });
+        try {
+            const data = await getRatingsByEvent(eventId);
+            set({ ratings: data, loading: false });
+        } catch (err) {
+            console.log(err);
+            set({error: "Failed to load ratings"});
+        } finally {
+            set({loading: false})
+        }
+    },
 }));
