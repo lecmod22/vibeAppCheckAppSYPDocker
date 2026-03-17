1541160348c2d7cd4d01e73f9fb07ab446995ed4
lecmod22 <lecmod22@htl-kaindorf.at>
2026-03-17 15:04:33 +0100

    test

--- backend/src/main/java/main/backend/Controller/ArtistController.java
+++ backend/src/main/java/main/backend/Controller/ArtistController.java
@@ -1,4 +1,29 @@
 package main.backend.Controller;
 
+import lombok.RequiredArgsConstructor;
+import main.backend.Dto.ArtistDto;
+import main.backend.Services.ArtistService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/artists")
+@RequiredArgsConstructor
 public class ArtistController {
+    private final ArtistService artistService;
+
+    @GetMapping
+    public List<ArtistDto> getAllArtists() {
+        return artistService.getAllArtists();
+    }
+
+    @GetMapping("/{artistId}")
+    public ResponseEntity<ArtistDto> getArtistById(@PathVariable Long artistId) {
+        return ResponseEntity.ok(artistService.getArtistById(artistId));
+    }
 }
